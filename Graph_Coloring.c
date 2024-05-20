c28a6881be022dc1c725636216d20589cadd2d73
Koushambha Das <122219149+KOUSHAMBHA15IT@users.noreply.github.com>
2024-05-21 01:48:14 +0530

    Update Graph_Coloring.c

--- Graph_Coloring.c
+++ Graph_Coloring.c
@@ -1,43 +1,37 @@
-#include<stdio.h>
-#define MAX 10
-int main()
-{
-	int a[MAX][MAX],color[MAX];
-	int i,j,n,p;
-	printf("Enter the no. of vertex ");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		printf("\n Neighbour of %c : ",i+65);
-		for(j=0;j<n;j++)
-		 scanf("%d",&a[i][j]);
-	}
-	printf("\n matrix form \n");
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n;j++)
-			printf("%d\t",a[i][j]);
-		printf("\n");
-	}
-	for(i=0;i<n;i++)
-	 color[i]=0; //initialize
-	 
-	color[0]=1; // initialize first node
-	p=1;
-	while(p<n)  //  5
-	{
-		color[p]=1;
-		for(j=0;j<p;j++)//  <5
-		{
-			if(a[p][j]==1 && color[j]==color[p])	
-			{
-				color[p]++;
-			}
-		}
-		p++;
-	}
-	for(i=0;i<n;i++)
-	{
-		printf("\n Vertex %c = color %d",i+65,color[i]);
-	}
+#include <stdio.h>
+#define MAX 6 // Adjust MAX according to the size of the matrix
+
+int main() {
+    int a[MAX][MAX] = {
+        {0, 1, 1, 0, 0, 0},
+        {1, 0, 0, 1, 1, 0},
+        {1, 0, 0, 1, 0, 1},
+        {0, 1, 1, 0, 0, 0},
+        {0, 1, 0, 0, 0, 1},
+        {0, 0, 1, 0, 1, 0}
+    };
+
+    int color[MAX] = {0}, n = MAX, i, j, p = 1;
+
+    printf("\nMatrix form:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++)
+            printf("%d\t", a[i][j]);
+        printf("\n");
+    }
+
+    color[0] = 1;
+    while (p < n) {
+        color[p] = 1;
+        for (j = 0; j < p; j++) {
+            if (a[p][j] == 1 && color[j] == color[p])
+                color[p]++;
+        }
+        p++;
+    }
+
+    for (i = 0; i < n; i++)
+        printf("\nVertex %c = color %d", i + 65, color[i]);
+
+    return 0;
 }
