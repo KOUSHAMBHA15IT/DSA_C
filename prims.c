d7931a93a2f7c42a4c04eae2504048b9e0f42336
Koushambha Das <122219149+KOUSHAMBHA15IT@users.noreply.github.com>
2024-05-23 18:41:28 +0530

    Update prims.c

--- prims.c
+++ prims.c
@@ -1,47 +1,40 @@
 #include<stdio.h>
 #define INF 9999
-int main()
-{
-	int n,i,j,s=0,r,c,p=0,min;
-	printf("Enter NUmber of Vertex ");
-	scanf("%d",&n);
-	int g[n][n];
-	int status[n];
-	for(i=0;i<n;i++)
-	{
-		printf("\n Enter Neighbour of %c = ",i+65);
-		for(j=0;j<n;j++)
-		{
-			scanf("%d",&g[i][j]);
-			if(g[i][j]==0)
-			 g[i][j]=INF;
-		}
-	}
-	for(i=0;i<n;i++)
-	 status[i]=0;
-	status[0]=1;// starting node
-	while(p<n-1)
-	{
-		for(i=0;i<n;i++)
-		{
-			if(status[i]==1)//process row
-			{
-				min=INF;
-				for(j=0;j<n;j++)
-				{
-					if(status[j]==0 && g[i][j]<min)
-					{
-						min=g[i][j];
-						r=i;
-						c=j;
-					}
-				}
-			}
-		}//outer for
-		printf("\n %c --> %c = %d",r+65,c+65,min);
-		s=s+min;
-		status[c]=1;
-		p++;
-	}
-	printf("\n Value %d",s);
+
+int main() {
+    int n = 5; // Number of vertices
+    int g[5][5] = {
+        {0, 2, 0, 6, 0},
+        {2, 0, 3, 8, 5},
+        {0, 3, 0, 0, 7},
+        {6, 8, 0, 0, 9},
+        {0, 5, 7, 9, 0}
+    };
+
+    int s = 0, r, c, p = 0, min;
+    int status[n];
+    
+    for (int i = 0; i < n; status[i++] = 0);
+    status[0] = 1; // Starting node
+    
+    while (p < n - 1) {
+        for (int i = 0; i < n; i++) {
+            if (status[i] == 1) {
+                min = INF;
+                for (int j = 0; j < n; j++) {
+                    if (status[j] == 0 && g[i][j] < min) {
+                        min = g[i][j];
+                        r = i;
+                        c = j;
+                    }
+                }
+            }
+        }
+        printf("\n %c --> %c = %d", r + 65, c + 65, min);
+        s += min;
+        status[c] = 1;
+        p++;
+    }
+    printf("\n Value %d", s);
+    return 0;
 }
